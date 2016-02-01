d980e7739a41397e8fae3e9900525a35bf424b5b
Benedikt Kristinsson <benedikt@inventati.org>
2016-02-01 17:20:13 +0100

    some stuff and now it works

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol, Factory
@@ -15,6 +16,11 @@ bootstrap_list = ["localhost:5008",
                   "localhost:5006",
                   "localhost:5005"]
 
+def _print(*args):
+    time = datetime.now().time().isoformat()[:8]
+    print time,
+    print " ".join(map(str, args))
+    
 class NCProtocol(Protocol):
     def __init__(self, factory, state="GETHELLO", kind="RECV"):
         self.factory = factory
@@ -23,7 +29,7 @@ class NCProtocol(Protocol):
         self.remote_nodeid = None
         self.kind = kind
         self.nodeid = self.factory.nodeid
-        self.lc_ping = LoopingCall(self.send_PING)
+        #self.lc_ping = LoopingCall(self.send_PING)
 
     def connectionMade(self):
         r_ip = self.transport.getPeer()
@@ -32,86 +38,84 @@ class NCProtocol(Protocol):
         self.host_ip = h_ip.host + ":" + str(h_ip.port)
 
     def print_peers(self):
-        print " [ ] STATE:", self.state
         if len(self.factory.peers) == 0:
-            print " [!] PEERS: No peers connected."
+            _print(" [!] PEERS: No peers connected.")
         else:
-            print " [ ] PEERS:"
+            _print(" [ ] PEERS:")
             for peer in self.factory.peers:
                 addr, kind = self.factory.peers[peer] 
-                print "     [*]", peer, addr, kind
+                _print("     [*]", addr, peer, kind)
 
     def write(self, line):
         self.transport.write(line + "\n")
 
     def connectionLost(self, reason):
-        print " [ ] LEAVES:", self.remote_nodeid
-        if self.remote_nodeid != self.nodeid:
-            try:
-                self.factory.peers.pop(self.remote_nodeid)
+        try:
+            self.factory.peers.pop(self.remote_nodeid)
+            if self.nodeid != self.remote_nodeid:
                 self.print_peers()
-            except KeyError:
-                print " [?] GHOST:", self.remote_nodeid, self.remote_ip
+        except KeyError:
+            if self.nodeid != self.remote_nodeid:
+                _print(" [ ] GHOST LEAVES:", self.remote_nodeid, self.remote_ip)
 
     def dataReceived(self, data):
-        if self.state != "READY":
+
+        if self.state in ["GETHELLO", "SENTHELLO"]:
             # Force first message to be HELLO or crash
             self.handle_HELLO(data)
         else:
-            print "else"
+            _print("else", data)
             envelope = messages.read_envelope(data)
             if envelope['msgtype'] == 'ping':
-                print "ping"
+                _print("ping")
                 self.handle_PING(data)
             elif envelope['msgtype'] == 'pong':
                 self.handle_PONG(data)
 
     def send_PING(self):
-        print " [ ] SEND_PING:", self.nodeid, "to", self.remote_nodeid
+        _print(" [ ] SEND_PING:", self.nodeid, "to", self.remote_nodeid)
         ping = messages.create_ping(self.nodeid)
         self.write(ping)
 
     def handle_PING(self, ping):
-        print " [ ] RECV_PING:", self.remote_nodeid
-        ping = messages.read_message(ping)
-        pong = messages.create_pong(self.nodeid, ping)
-        print " [ ] SEND_PONG"
-        self.write(pong)
+        _print(" [ ] RECV_PING:", self.remote_nodeid)
+        if messages.read_message(ping):
+            pong = messages.create_pong(self.nodeid)
+            _print(" [ ] SEND_PONG")
+            self.write(pong)
 
     def handle_PONG(self, pong):
-        # TODO: somehow get the ping nonce and check?
         pong = messages.read_message(pong)
 
     def send_HELLO(self):
         hello = messages.create_hello(self.nodeid, self.VERSION)
-        print " [ ] SEND_HELLO:", self.nodeid, "to", self.remote_ip
+        #_print(" [ ] SEND_HELLO:", self.nodeid, "to", self.remote_ip)
         self.transport.write(hello + "\n")
-        self.state = "GETHELLO"
+        self.state = "SENTHELLO"
         
     def handle_HELLO(self, hello):
         try:
             hello = messages.read_message(hello)
             self.remote_nodeid = hello['nodeid']
             if self.remote_nodeid == self.nodeid:
-                print " [!] Dropping connection to self on", self.host_ip
+                _print(" [!] Found myself at", self.host_ip)
                 self.transport.loseConnection()
             else:
-                my_hello = messages.create_hello(self.nodeid, self.VERSION)
-                self.transport.write(my_hello + "\n")
+                if self.state == "GETHELLO":
+                    my_hello = messages.create_hello(self.nodeid, self.VERSION)
+                    self.transport.write(my_hello + "\n")
                 self.factory.peers[self.remote_nodeid] = (self.remote_ip, self.kind)
                 self.state = "READY"
-                print " [ ] JOINED:", self.remote_nodeid
                 self.print_peers()
-                self.lc_ping.start(4.20)
+                #self.lc_ping.start(4.20)
         except (ValueError, ):
-            print " [!] Disconnecting peer. ",
-            print "Unable to read hello msg from", self.remote_ip
+            _print(" [!] ERROR: Malformed hello from", self.remote_ip)
             self.transport.loseConnection()
         except messages.InvalidSignatureError:
-            print " [!] Disconnecting peer. ",
-            print "Invalid signature in hello from", self.remote_ip
+            _print(" [!] ERROR: Invalid hello sign ", self.remoteip)
             self.transport.loseConnection()
 
+# Splitinto NCRecvFactory and NCSendFactory (also reconsider the names...:/)
 class NCFactory(Factory):
     def __init__(self):
         pass
@@ -120,7 +124,7 @@ class NCFactory(Factory):
         self.peers = {}
         self.numProtocols = 0
         self.nodeid = cryptotools.generate_nodeid()[:10]
-        print " [ ] NODEID:", self.nodeid
+        _print(" [ ] NODEID:", self.nodeid)
 
     def stopFactory(self):
         pass
@@ -140,18 +144,18 @@ if __name__ == "__main__":
         port = 5005
     try:
         endpoint = TCP4ServerEndpoint(reactor, port)
-        print " [ ] LISTEN:", port
+        _print(" [ ] LISTEN:", port)
         ncfactory = NCFactory()
         endpoint.listen(ncfactory)
     except CannotListenError:
-        print "[!] Address in use"
+        _print("[!] Address in use")
         sys.exit(1)
 
     
     # connect to bootstrap addresses
-    print " [ ] Trying to connect to bootstrap hosts:"
+    _print(" [ ] Trying to connect to bootstrap hosts:")
     for bootstrap in bootstrap_list:
-        print "     [*]", bootstrap
+        _print("     [*]", bootstrap)
         host, port = bootstrap.split(":")
         point = TCP4ClientEndpoint(reactor, host, int(port))
         d = connectProtocol(point, NCProtocol(ncfactory, "SENDHELLO", "SEND"))
